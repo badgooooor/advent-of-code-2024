62aeb0670dda95fefdbc7cf707c085c6dcf0b64c
Yuttakhan B <yuttakhanbaingen@gmail.com>
2024-12-04 13:34:37 +0700

    Add solution for day 1 part 2

--- src/day-1/main.rs
+++ src/day-1/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{self, BufRead};
 
@@ -27,12 +28,18 @@ fn main() -> std::io::Result<()> {
     first_array.sort_by(|a, b| a.abs().cmp(&b.abs()));
     second_array.sort_by(|a, b| a.abs().cmp(&b.abs()));
 
+    // Solve part 1
     let result_1 = solve_1(&first_array, &second_array);
     match result_1 {
         Ok(total) => println!("Result 1: {}", total),
-        _ => println!("Something wrong"),
+        _ => println!("Something wrong in result 1"),
     }
 
+    let result_2 = solve_2(&first_array, &second_array);
+    match result_2 {
+        Ok(total) => println!("Result 2: {}", total),
+        _ => println!("Something wrong in result 2"),
+    }
     Ok(())
 }
 
@@ -49,4 +56,39 @@ fn solve_1(first_array: &Vec<i32>, second_array: &Vec<i32>) -> Result<i32, i32> 
     }
 
     return Ok(total);
+}
+
+fn solve_2(first_array: &Vec<i32>, second_array: &Vec<i32>) -> Result<i32, i32> {
+    let mut total: i32 = 0;
+
+    if first_array.len() != second_array.len() {
+        return Err(0);
+    }
+
+    let freq_map = count_frequencies(second_array);
+
+    for i in 0..first_array.len() {
+        match freq_map.get(&first_array[i]) {
+            Some(item_frequency) => {
+                let item_product = first_array[i] * item_frequency;
+                total += item_product;
+            }
+            None => {
+                total += 0;
+            }
+        }
+    }
+
+    return Ok(total);
+}
+
+// Utilities: Counting frequency
+fn count_frequencies(numbers: &Vec<i32>) -> HashMap<i32, i32> {
+    let mut map = HashMap::new();
+
+    for &num in numbers {
+        *map.entry(num).or_insert(0) += 1;
+    }
+
+    map
 }
