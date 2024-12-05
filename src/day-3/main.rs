8c77c4c07372a6fc1a847b21fdf160a6d4480c74
Yuttakhan B <yuttakhanbaingen@gmail.com>
2024-12-05 10:03:03 +0700

    Add solution for day 3 part 2

--- src/day-3/main.rs
+++ src/day-3/main.rs
@@ -7,8 +7,8 @@ fn main() -> std::io::Result<()> {
 
     // Read line
     let content = fs::read_to_string(file_location_arg)?;
-    println!("Contents: {}", content);
 
+    // Extract from line
     let multiples = extract_multiples(&content);
 
     // Solve part 1
@@ -18,35 +18,64 @@ fn main() -> std::io::Result<()> {
         _ => println!("Something wrong in result 1"),
     }
 
+    // Solve part 2
+    let result_2 = solve_2(&content);
+    match result_2 {
+        Ok(total) => println!("Result 2: {}", total),
+        _ => println!("Something wrong in result 2"),
+    }
+
     Ok(())
 }
 
-fn solve_1(multiples: &Vec<(i32, i32)>) -> Result<i32, i32> {
-    let total: i32 = multiples.iter().map(|(x, y)| x * y).sum();
+fn solve_1(multiples: &Vec<(i32, i32, i32)>) -> Result<i32, i32> {
+    let total: i32 = multiples.iter().map(|(x, y, _)| x * y).sum();
 
     Ok(total)
 }
 
+fn solve_2(content: &String) -> Result<i32, i32> {
+    let re = Regex::new(r"(mul\((?<op1>[0-9]+),(?<op2>[0-9]+)\))|(?<dt>don't\(\))|(?<d>do\(\))")
+        .unwrap();
+
+    let mut res = 0;
+    let mut valid = true;
+    for m in re.captures_iter(content) {
+        if let Some(_) = m.name("d") {
+            valid = true;
+        } else if let Some(_) = m.name("dt") {
+            valid = false;
+        }
+        if valid {
+            if let (Some(op1), Some(op2)) = (m.name("op1"), m.name("op2")) {
+                res += op1.as_str().parse::<i32>().unwrap() * op2.as_str().parse::<i32>().unwrap();
+            }
+        }
+    }
+
+    Ok(res)
+}
+
 // Utility: Extract numbers from pattern
-fn extract_multiples(text: &String) -> Vec<(i32, i32)> {
+fn extract_multiples(text: &String) -> Vec<(i32, i32, i32)> {
     let pattern = r"mul\((\d+),(\d+)\)";
     let re = Regex::new(pattern).unwrap();
 
-    let mut multiples: Vec<(i32, i32)> = Vec::new();
+    let mut multiples: Vec<(i32, i32, i32)> = Vec::new();
     // Find all matches
     for capture in re.find_iter(&text) {
         let line_str = capture.as_str();
-        println!("Found match: {}", line_str);
-
         let numbers = &line_str[4..line_str.len() - 1];
         let mut parts = numbers.split(',');
 
         let first_number: i32 = parts.next().unwrap().parse().unwrap();
         let second_number: i32 = parts.next().unwrap().parse().unwrap();
 
-        println!("Numbers: {} {}", first_number, second_number);
-
-        multiples.push((first_number, second_number));
+        multiples.push((
+            first_number,
+            second_number,
+            i32::try_from(capture.start()).unwrap(),
+        ));
     }
 
     multiples
